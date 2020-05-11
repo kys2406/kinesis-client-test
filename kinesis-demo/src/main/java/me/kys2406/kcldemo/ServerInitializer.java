30b79a22c4dd9c480d31939cc045d8a03e4e92b9
고윤석/검색개발팀 <yunseok.ko@musinsa.com>
2020-05-11 12:10:44 +0900

    multi stream 추가 테스트 진행

--- kinesis-demo/src/main/java/me/kys2406/kcldemo/ServerInitializer.java
+++ kinesis-demo/src/main/java/me/kys2406/kcldemo/ServerInitializer.java
@@ -3,6 +3,7 @@ package me.kys2406.kcldemo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.kys2406.kcldemo.sync.SyncService;
+import me.kys2406.kcldemo.sync.SyncService2;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -14,9 +15,13 @@ public class ServerInitializer implements ApplicationRunner {
 
     private final SyncService syncService;
 
+    private final SyncService2 syncService2;
+
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
         syncService.resister();
+
+        syncService2.resister();
 //        StopWatch stopWatch = new StopWatch();
 //
 //        ExecutorService executorService = Executors.newFixedThreadPool(8);
